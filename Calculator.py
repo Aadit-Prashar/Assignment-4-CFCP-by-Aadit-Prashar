170da9d2f939f4873231be701ea301b4f4ca35de
Aadit-Prashar <prasharaadit@gmail.com>
2025-11-22 16:46:18 +0530

    third commit

--- Calculator.py
+++ Calculator.py
@@ -2,3 +2,11 @@ print("Hello, user!")
 #Enter the numbers:
 num1=int(input("Enter first number:"))
 num2=int(input("Enter second number:"))
+
+#Choices for arithematic calculations
+print("Select 1 for addition")
+print("Select 2 for subtraction")
+print("Select 3 for multiplicaion")
+print("Select 4 for division")
+
+choices=int(input("Enter your selected choice:"))
