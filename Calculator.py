ea49e832c66c124ad8d81d41807bbb1d66419ec6
Aadit-Prashar <prasharaadit@gmail.com>
2025-11-22 16:48:20 +0530

    forth commit

--- Calculator.py
+++ Calculator.py
@@ -8,5 +8,23 @@ print("Select 1 for addition")
 print("Select 2 for subtraction")
 print("Select 3 for multiplicaion")
 print("Select 4 for division")
-
 choices=int(input("Enter your selected choice:"))
+#conditions
+if choices=='1' :
+    sum=num1+num2
+    print("On adding numbers we get:",sum)
+
+elif(choices=='2'):
+    diff=num1-num2
+    print("On subtracting numbers we get:",diff)
+
+elif(choices=='3'):
+    product=num1*num2
+    print("On multiplying numbers we get:",product)
+
+elif(choices=='4'):
+    quotient=num1//num2
+    print("On dividing numbers we get:",quotient)
+
+else:
+    print("INVALID CHOICE!! Please select from above choices")
